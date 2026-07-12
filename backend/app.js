e53b5802fd8e15d5ca051764d443b8a83db662c7
Neha-vundavilli2710 <23p31a4272@acet.ac.in>
2026-07-13 00:06:22 +0530

    fix: Update CORS configuration to allow all origins for flexibility

--- backend/app.js
+++ backend/app.js
@@ -13,10 +13,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://restaurant-reservation-management-system-rrd8.onrender.com/api"
-    ],
+    origin: true,
     credentials: true
 }));
 
